568f2117e7deb9e9b089f7c45b84f39fe3137482
Солодухин Денис Вячеславович <d_solodukhin@AT.local>
2019-09-09 12:08:47 +0300

    isPowerOfThree

--- trials.php
+++ trials.php
@@ -15,3 +15,22 @@ function binarySum ($a, $b){
   $result = decbin($result);
   return $result;
 }
+
+// Реализуйте функцию isPowerOfThree которая определяет, является ли переданное число натуральной степенью тройки. 
+// Например число 27 это третья степень (33), а 81 это четвертая (34).
+
+
+// isPowerOfThree(1); // → true (3^0)
+// isPowerOfThree(3); // → true
+// isPowerOfThree(4); // → false
+// isPowerOfThree(9); // → true
+
+function isPowerOfThree ($num) {
+  if ($num == 1 ) {
+    return true;
+  } else if ($num % 3 == 0 ) {
+    return true;
+  } else {
+    return false;
+  }
+}
