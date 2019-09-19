b30acd6d927f6e00814091c5384ea9c128fbeae4
Солодухин Денис Вячеславович <d_solodukhin@AT.local>
2019-09-19 17:14:11 +0300

    add func long string

--- longestLength.php
+++ longestLength.php
@@ -9,26 +9,71 @@
 // longestLength('abcdeef'); // → 5
 // longestLength('jabjcdel'); // → 7 
 
+//=========================================================================================
+//=========================================================================================
+//=========================================================================================
 
-// пока не могу решить...
 
-$str = 'abcddef';
+//=======================================MY SOLUTION ==================================================
 
-function longestLength ($str){
+
+
+// BEGIN (write your solution here)
+function longestLength ($str) {
     $length = strlen($str);
-    $latters = [];
+    $count = 0;
     
-    for ($i = 0; $i < $length; $i++){
-        if (!in_array($str[$i], $latters)) {
-            $latters[] = $str[$i];
+    for($i = 0; $i < $length; $i++){
+        $result = longStr($str, $i);
+        if ($count < $result) {
+            $count = $result;
         }
     }
-    $result = count($latters);
-    
-    return $result;
+    return $count;
 }
 
 
-$a = longestLength($str);
-print_r($a);
 
+function longStr(string $str, int $j){
+    $len = strlen($str);
+    $arrStr = [];
+    while ($j < $len) {
+        if(!in_array($str[$j], $arrStr)){
+            $arrStr[] = $str[$j];
+            $j++;
+        } else {
+             return count($arrStr);
+        }  
+        
+    }
+    return count($arrStr);
+}
+// END
+
+//=======================================MY SOLUTION ==================================================
+
+
+//=======================================Teacher SOLUTION ==================================================
+
+// BEGIN
+function isUniqueString(string $string)
+{
+    return count(array_flip(str_split($string))) === strlen($string);
+};
+
+function longestLength(string $str)
+{
+    $length = strlen($str);
+    for ($i = $length - 1; $i >= 0; $i -= 1) {
+        for ($j = 0; $j < $length - $i; $j += 1) {
+            $sub = substr($str, $j, $i + 1);
+            if (isUniqueString($sub)) {
+                return $i + 1;
+            }
+        }
+    }
+    return 0;
+}
+// END
+
+//=======================================Teacher SOLUTION ==================================================
