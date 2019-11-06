a5d47add76d56a5cb2c8e3d695b58b3462bf9f92
Солодухин Денис Вячеславович <d_solodukhin@AT.local>
2019-11-06 10:54:01 +0300

    add fluent interface directory and start testing solution

---
+++ oop_design/Fluent Interface/test.php
@@ -0,0 +1,64 @@
+<?php
+
+  $raw = [
+      [
+          'name' => 'istambul',
+          'country' => 'turkey'
+      ],
+      [
+          'name' => 'Moscow ',
+          'country' => ' Russia'
+      ],
+      [
+          'name' => 'iStambul',
+          'country' => 'tUrkey'
+      ],
+      [
+          'name' => 'antalia',
+          'country' => 'turkeY '
+      ],
+      [
+          'name' => 'samarA',
+          'country' => '  ruSsiA'
+      ],
+      [
+          'name' => 'istambul',
+          'country' => 'usa'
+      ],
+  ];
+
+$maping = array_map(function ($value){
+  $result = [];
+  $result['name'] = trim(strtolower($value['name']));
+  $result['country'] = trim(strtolower($value['country']));
+  return $result;
+}, $raw);
+
+echo "<pre>";
+print_r($maping);
+echo "</pre>";
+
+$reduced = array_reduce($maping, function ($acc, $vol) {
+if(!array_key_exists($vol['country'], $acc)) {
+  $acc[$vol['country']] = [];
+}
+$acc[$vol['country']][] = $vol['name'];
+return $acc;
+}, []);
+
+echo "<pre>";
+print_r($reduced);
+echo "</pre>";
+
+function myUnique($reduced)
+{
+  foreach ($reduced as $key => $vol){
+    $reduced[$key] =  array_unique($vol);
+  }
+  return $reduced;
+}
+
+echo "<pre>";
+print_r(myUnique($reduced));
+echo "</pre>";
+// еще нужно отсортировать массивы по алфавиту и ключи и значения
