daa871f8e9a5eab2ad1ccdc41a6b674e7876840b
Denis <den-sidnay@yandex.ru>
2019-11-23 20:50:21 +0300

    add function Normileze

--- oop_design/Fluent Interface/normalize.php
+++ oop_design/Fluent Interface/normalize.php
@@ -2,8 +2,98 @@
 
 require_once '../../vendor/autoload.php';
 
-$a = collect([1,2,3,4,5]);
+
+$raw = [
+    [
+        'name' => 'pariS ',
+        'country' => ' france'
+    ],
+    [
+        'name' => ' madrid',
+        'country' => ' sPain'
+    ],
+    [
+        'name' => 'valencia',
+        'country' => 'spain'
+    ],
+    [
+        'name' => 'marcel',
+        'country' => 'france'
+    ],
+    [
+        'name' => ' madrid',
+        'country' => ' sPain'
+    ],
+];
+
+// function normalize($raw)
+// {
+// 	$collect = collect($raw)->map( function ($arr) {
+// 	return array_map( function ($item) {
+// 		return trim(strtolower($item));
+// 	}, $arr); 
+// } )->reduce(function ($acc, $item) {
+// 	if (!array_key_exists($item['country'], $acc)) {
+// 		$acc[$item['country']] = [];
+// 	}
+// 	$acc[$item['country']][] = $item['name'];
+// 	return $acc;
+// }, []);
+
+//   foreach ($collect as $key => $vol){
+//     sort($vol);
+//     $collect[$key] = array_values(array_unique($vol));
+//   }
+//   ksort($collect);
+//   return $collect;
+// }
+
+// echo "<pre>";
+// print_r(normalize($raw));
+// echo "</pre>";
+
+// ======================= hexlet solution ============================
+
+// BEGIN
+// function normalize2($raw)
+// {
+//     return collect($raw)
+//         ->map(function ($value) {
+//             return array_map('mb_strtolower', $value);
+//         })
+//         ->map(function ($value) {
+//             return array_map('trim', $value);
+//         })
+//         ->mapToGroups(function ($item, $key) {
+//             return [$item['country'] => $item['name']];
+//         })
+//         ->map(function ($cities) {
+//             return $cities->unique()->sort()->values();
+//         })
+//         ->sortKeys()
+//         ->toArray();
+// }
+// END
+
+// ===================== my experement for collection lib =================
+
+function myCollect($raw)
+{
+	return collect($raw)
+		->map( function ($value){
+			return array_map('mb_strtolower', $value);
+		})
+		->map(function ($value) {
+			return array_map('trim', $value);
+		})
+        ->mapToGroups(function ($item, $key) {
+            return [$item['country'] => $item['name']];
+        })
+        ->map(function ($cities) {
+            return $cities->unique()->sort()->values();
+        });
+}
 
 echo "<pre>";
-print_r($a);
+print_r(myCollect($raw));
 echo "</pre>";
