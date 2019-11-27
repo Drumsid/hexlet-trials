7a74ffc9b09d150d71d587e8a14b8e857cf9f02e
Солодухин Денис Вячеславович <d_solodukhin@AT.local>
2019-11-27 12:28:15 +0300

    add dir drunkard

---
+++ oop_design/Drunkard/Drunkard .php
@@ -0,0 +1,22 @@
+<?php
+
+require_once '../../vendor/autoload.php';
+
+use Ds\Deque;
+
+class Drunkard
+{
+
+    public function __construct()
+    {
+       
+    }
+
+    public function run($firstPlayer, $secondPlayer)
+    {
+        $firstPlayer = new Deque();
+        $secondPlayer = new Deque();
+    }
+
+    
+}
