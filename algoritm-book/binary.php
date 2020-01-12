a5a7cd870083d560ca16d3fec97151cec41fd966
Denis <den-sidnay@yandex.ru>
2020-01-12 22:31:32 +0300

    add binary search function

---
+++ algoritm-book/binary.php
@@ -0,0 +1,36 @@
+<?php
+
+// Реализация функции бинарного поиска в ОСОРТИРОВАННОМ списке (массиве)
+
+$arr = [2, 5, 7, 9, 12, 14, 16, 18, 19, 24, 27, 59];
+
+function rounEven($num)
+{
+    if ($num % 2 !== 0) {
+        return $num - 1;
+    }
+    return $num;
+}
+
+function binary($arr, $search)
+{
+    $low = 0;
+    $hight = count($arr) - 1;
+
+    while ($low <= $hight) {
+        $mid = (rounEven($low + $hight)) / 2;
+        $guess = $arr[$mid];
+        if ($guess == $search) {
+            return $mid;
+        }
+        if ($guess > $search) {
+            $hight = $mid - 1;
+        }
+        if ($guess < $search) {
+            $low = $mid + 1;
+        }
+    }
+    return null;
+}
+
+var_dump(binary($arr, 59));
